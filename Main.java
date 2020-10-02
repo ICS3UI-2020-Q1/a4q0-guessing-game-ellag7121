6cf06c994d2789998d7c5cf9e589b44a49aa74ea
ellag7121 <ellag7121@wrdsb.ca>
2020-10-02 14:45:03 +0000

    finished guessing game

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+import java.util.Random;
 /**
- *
- * @author 
+ * make a guessing game, where a random # is chosen, then the user guesses and the program tells them if it's lower or highe than the chosen # until it is guessed
+ * @author Graham Ellacott
  */
 public class Main {
 
@@ -9,7 +11,30 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // Create a scanner to get user input
+    Scanner input = new Scanner(System.in);
+
+    //Create a random number generator
+    Random rand = new Random();
+    //choose a random number
+    int randomNumber = rand.nextInt(100) + 1;
+
+    //the variable to store the user's guess
+    int guess;
+
+    //start the guessing loop
+    do{
+      //ask user for guess
+      System.out.println("Please enter your guess between 1 and 100");
+      guess = input.nextInt();
+
+      if(guess == randomNumber){ //check if number is correct
+        System.out.println("You are correct! Good job!");
+      }else if(guess < randomNumber){ //if the guess is less than the random number
+        System.out.println("You are too low. Try again.!"); //tell the user it's too low
+      }else{ //if the guess is more than the random number
+        System.out.println("You are too high. Try again.!"); //tell the user it's too high
+      }
+    }while(guess != randomNumber); //if the guess is incorrect, repeat
   }
 }
